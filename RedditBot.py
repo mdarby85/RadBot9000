98e031fbf7ea8f02f8307430a5fbde7254cb37fa
Matthew <matthew_darby@baylor.edu>
2019-03-09 00:24:11 -0600

    Added reply only to new posts by submission ID

--- RedditBot.py
+++ RedditBot.py
@@ -1,22 +1,31 @@
 import praw
 import config
+import os
 
 def bot_login():
-    print "Logging in..."
-    r = praw.Reddit(username = config.username,
+    print ("Logging in...")
+    reddit = praw.Reddit(username = config.username,
             password = config.password,
             client_id = config.client_id,
             client_secret = config.client_secret,
             user_agent = "RadBot9000 v0.1")
-    print "Logged in!"
-    return r
+    print ("Logged in!")
+    return reddit
 
-def run_bot(r):
+def run_bot(reddit):
     processed = []
-    for submission in r.subreddit('2Rad4Radio').new(limit = 5):
+
+    for submission in reddit.subreddit('2Rad4Radio').new(limit = 5):
+        with open("/home/matt/Bots/RadBot/replied.txt", "r") as f:
+            processed = f.read()
+            processed = processed.split("\n")
+            processed = list(filter(None, processed))
         if submission.id not in processed:
-            submission.reply("Rad")
+            submission.reply('Rad').mod.distinguish(sticky=True)
+            processed.append(submission.id)
+            with open("/home/matt/Bots/RadBot/replied.txt", "w") as f:
+                for post_id in processed:
+                    f.write(post_id + "\n")
 
-
-r = bot_login()
-run_bot(r)
+reddit = bot_login()
+run_bot(reddit)
